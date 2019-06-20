e64c9c0f00b8b78204a47113e106a8339ac6de89
Artem-Efremov <efremov.ar.ev@gmail.com>
2019-06-20 20:00:47 +0300

    Create second test case

--- async_requests/test_models.py
+++ async_requests/test_models.py
@@ -1,6 +1,7 @@
 import unittest
 import models
 
+
 class TestFakeName(unittest.TestCase):
 
     def test_is_valid_name(self):
@@ -22,6 +23,19 @@ class TestFakeName(unittest.TestCase):
         for name in invalid_names:
             with self.subTest(name=name):
                 self.assertFalse(models.FakeName.is_valid_name(name))
+
+    def test_split_fullname(self):
+
+        fullname_examples = [
+            ('Dr. Eldred Crooks DDS', ['Eldred', 'Crooks']),
+            ('Prof. Taya Kassulke', ['Taya', 'Kassulke']),
+            ('Mister Casimir Harvey', ['Mister', 'Casimir', 'Harvey'])
+        ]
+
+        for fullname, valid_answer in fullname_examples:
+            with self.subTest(fullname=fullname, valid_answer=valid_answer):
+                test_obj = models.FakeName(fullname=fullname)
+                self.assertEqual(sorted(test_obj.split_fullname()), sorted(valid_answer))
 
 
 if __name__ == "__main__":
